73416a5185db1ba48f6af52dae1579b26c3b29b7
LuisBBandeira <luisfilipebentobandeira@gmail.com>
2025-03-24 21:31:43 +0000

    fix:anime[id] TypeScript error

--- neko_vibe/src/app/anime/[id]/page.tsx
+++ neko_vibe/src/app/anime/[id]/page.tsx
@@ -2,11 +2,7 @@ import { AnimeService, Anime } from "../../services";
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default async function AnimeDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function AnimeDetailPage({params,}: {params: Promise<{ id: string }>;}) {
 
   // For some reason, the await has no effet but if i don't put it here a error will be thrown!
   const  resolvedParams = await params;
